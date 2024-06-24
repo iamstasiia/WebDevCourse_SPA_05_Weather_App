6d7ad691e4cda1550931d1231df44b2c8bc573b7
Anastasiia <anastasiia.khrypko@dci-student.org>
2024-06-24 09:51:16 +0200

    header is formatted

--- src/components/header/Header.comp.jsx
+++ src/components/header/Header.comp.jsx
@@ -1,16 +1,19 @@
 import LogoComponent from "./Logo.comp.jsx";
 import CityComponent from "./City.comp.jsx";
 import Search from "./Search.comp.jsx";
-import './Header.style.scss';
+import "./Header.style.scss";
 
 const HeaderComponent = ({ setLocation, weatherData, currentDate }) => {
-  return (
-    <header>
-      <LogoComponent />
-      <CityComponent weatherData={weatherData} currentDate={currentDate} />
-        <Search setLocation={setLocation} /> 
-    </header>
-  );
+    return (
+        <header>
+            <LogoComponent />
+            <CityComponent
+                weatherData={weatherData}
+                currentDate={currentDate}
+            />
+            <Search setLocation={setLocation} />
+        </header>
+    );
 };
 
 export default HeaderComponent;
