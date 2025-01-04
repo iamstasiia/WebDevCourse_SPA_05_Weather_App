a0c76b1780a4ce7847f3ff2c1a257480f9b7751b
Anastasiia <a.khrypko.work@gmail.com>
2025-01-04 21:10:06 +0100

    prettier applied

--- src/functions/fetchWeatherData.func.js
+++ src/functions/fetchWeatherData.func.js
@@ -6,9 +6,7 @@ export const fetchWeatherData = async (location) => {
     try {
         const response = await fetch(url);
         if (!response.ok) {
-            throw new Error(
-                `Network response was not ok: ${response.statusText}`,
-            );
+            throw new Error(`Network response was not ok: ${response.statusText}`);
         }
         const data = await response.json();
         return data;
